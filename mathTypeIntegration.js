75ef9842ece1a554842ccc64f2ad5e079b31d3d8
Vikram Singh <vikram.singh3@pw.live>
2023-03-27 12:35:23 +0530

    updated integation doc

--- mathTypeIntegration.js
+++ mathTypeIntegration.js
@@ -6,33 +6,34 @@ function getImageHTML(data) {
     "'"
   )}" role="math" style="max-width: none; vertical-align: -4px;">`;
 }
-function closeIframe() {
-  const mathTypeWindow = document.getElementById("mathType");
+function closeIframe(editorId, eventHandler) {
+  if (eventHandler) {
+    window.removeEventListener("message", eventHandler);
+  }
+  var mathTypeWindow = document.getElementById("mathType-" + editorId);
   mathTypeWindow.style.display = "none";
 }
 
-function openIFrame() {
-  const mathTypeWindow = document.getElementById("mathType");
+function openIFrame(editorId) {
+  var mathTypeWindow = document.getElementById("mathType-" + editorId);
   mathTypeWindow.style.display = "block";
   mathTypeWindow.style =
     "bottom: 0px;right: 10px;height: 422px;width: 700px;border: 1px solid lightgrey;background: #fafafa;z-index: 999999;position: fixed;bottom: 3px;right: 3px;box-shadow: rgb(0 0 0 / 16%) 0px 3px 8px 6px;display: block; border-radius: 3%;";
 }
 
-let isPluginCommSet = false;
-closeIframe();
+var isPluginCommSet = false;
 
 function setIframeCommunication(editor) {
-  const iFrame = window.frames.mathType;
+  var iFrame = window.frames["mathType-" + editor.id];
   iFrame.postMessage({ action: "setCommunication", data: "" }, "*");
-  if (!isPluginCommSet) {
-    isPluginCommSet = true;
-    window.addEventListener("message", (event) => {
+  function attachEventListener(editor) {
+    window.addEventListener("message", function eventHandler(event) {
       switch (event.data.action) {
         case "closeIFrame":
-          closeIframe();
+          closeIframe(editor.id, eventHandler);
           break;
         case "openIFrame":
-          openIFrame();
+          openIFrame(editor.id);
           break;
         case "insertImage":
           editor.insertContent(getImageHTML(event.data.data));
@@ -45,20 +46,23 @@ function setIframeCommunication(editor) {
                 },
                 "*"
               );
+              attachEventListener(editor);
             }
           });
-          closeIframe();
+          closeIframe(editor.id, eventHandler);
           break;
       }
     });
   }
+  attachEventListener(editor);
 }
 
 tinymce.PluginManager.add("MathType", function (editor) {
+  closeIframe(editor.id);
   editor.ui.registry.addButton("mathtype", {
     text: "MathType",
     onAction: () => {
-      openIFrame();
+      openIFrame(editor.id);
       setIframeCommunication(editor);
     },
   });
